ec2a0b12657921513cccd9f05291c4c6b57fe18f
riski_putra <riski.putra@stickearn.com>
2021-02-21 20:03:25 +0700

    Upload avatar endpoint

--- user/service.go
+++ user/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
 	IsEmailAvailabel(input CheckEmailInput) (bool, error)
+	SaveAvatar(ID int, fileLocation string) (User, error)
 }
 
 type service struct {
@@ -76,4 +77,24 @@ func (s *service) IsEmailAvailabel(input CheckEmailInput) (bool, error) {
 	}
 
 	return false, nil
+}
+
+func (s *service) SaveAvatar(ID int, fileLocation string) (User, error) {
+	// dapatkan user berdasarkan ID
+	// update attribute avatar filename
+	// simpan parubahan avatar file name
+
+	user, err := s.repository.FindById(ID)
+	if err != nil {
+		return user, err
+	}
+
+	user.AvatarFileName = fileLocation
+
+	updatedUser, err := s.repository.Update(user)
+	if err != nil {
+		return updatedUser, err
+	}
+
+	return updatedUser, nil
 }
